44c4c3166b6cc388276ffa85e524a45e7155a4da
nataliapovaliy <natalia.bg1125@gmail.com>
2022-12-15 23:08:46 +0200

    01-gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,30 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+const listGallery = document.querySelector(".gallery");
+
+const listImage = galleryItems
+listImage.map(item => {
+
+    const listGalleryElementLink = document.createElement('a');
+    listGalleryElementLink.className = "gallery__item";
+    listGalleryElementLink.href = `${item.original}`;
+    listGallery.append(listGalleryElementLink);
+
+    const listGalleryElementImg = document.createElement('img');
+    listGalleryElementImg.className = "gallery__image";
+    listGalleryElementImg.src = `${item.preview}`;
+    listGalleryElementImg.alt = `${item.description}`;
+    listGalleryElementLink.append(listGalleryElementImg);
+});
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
 
+const player = new Player('handstick', {
+    id: 19231868,
+    width: 640
+});
+
+player.on('play', function() {
+    console.log('played the video!');
+});
+
+const playerVimeo = document.querySelector("#vimeo-player");
+
+const onPlay = function(data) {
+    const timeCurrent = playerVimeo.setItem("videoplayer-current-time", data);
+};
+
+player.on('play', onPlay);
+
+player.setCurrentTime(30.456).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
